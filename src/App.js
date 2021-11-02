1a460d5230580ebfc513ceaab0f0facffec1ddda
Lunkaz <marialbornozapata@gmail.com>
2021-11-01 23:41:43 -0300

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup} from '@material-ui/core';
+import { Button, ButtonGroup, Checkbox, FormControlLabel } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return(
+    <FormControlLabel
+      control={
+        <Checkbox
+        checked={checked}
+        icon={<DeleteIcon />}
+        checkedIcon={<SaveIcon/>}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          "aria-label": "secondary checbox"
+        }}
+      />
+      }
+      label="Testing Checkbox"
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" color="primary">
           <Button
             startIcon={<SaveIcon />}
